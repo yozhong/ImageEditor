e10c002de3559621354f9a94f432257c85705a4e
yozhong <yozhong@gmail.com>
2022-12-12 13:16:49 +0800

    Add affine plugin

---
+++ AffinePlugin/affine_plugin.h
@@ -0,0 +1,19 @@
+#ifndef AFFINEPLUGIN_H
+#define AFFINEPLUGIN_H
+
+#include <QObject>
+#include <QtPlugin>
+
+#include "editor_plugin_interface.h"
+
+class AffinePlugin: public QObject, public EditorPluginInterface
+{
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID EDIT_PLUGIN_INTERFACE_IID);
+    Q_INTERFACES(EditorPluginInterface);
+public:
+    QString name();
+    void edit(const cv::Mat &input, cv::Mat &output);
+};
+
+#endif // AFFINEPLUGIN_H
